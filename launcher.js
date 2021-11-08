4cede5834e9c10d133ea5d21ff5bdd96ca7579d1
stranger20008 <78595730+stranger20008@users.noreply.github.com>
2021-11-08 15:00:40 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,45 @@
+class Launcher {
+
+    constructor(body,anchor){
+    var options = {
+        bodyA : body,
+        pointB : anchor,
+        stiffness : 0.004,
+        length : 10
+
+
+    }
+    this.bodyA = body
+    this.pointB = anchor
+    this.launcher = Constraint.create(options)
+    World.add(world,this.launcher)
+
+
+
+    }
+    fly(){
+        this.launcher.bodyA=null
+    }
+    attach(body){
+        this.launcher.bodyA = body
+    }
+    display(){
+        if(this.launcher.bodyA){
+            var pointA = this.bodyA.position
+            var pointB = this.pointB
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+    }
+    
+
+
+
+    
+    
+
+    
+
+
+
+
+}
